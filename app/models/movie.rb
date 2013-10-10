37515bce8f63a144e23cf78f6a8fccbb91c4ec68
Michael Ornellas <mike@themakersquare.com>
2013-10-09 23:53:04 -0500

    refactoring average_rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,18 +14,17 @@ class Movie < ActiveRecord::Base
   end
 
   def self.average_rating
-    movie_scores = []
-
-    self.all.each do |movie|
-      movie_scores << movie.audience_rating if movie.audience_rating
+    scores = self.all.collect do |movie|
+      movie.audience_rating
     end
 
-    if movie_scores.length > 0
-      movie_scores.inject(:+).to_f / movie_scores.length
+    scores.compact!
+
+    if scores.length > 0
+      scores.sum / scores.length
     else
       nil
     end
-
   end
 
   def snippet
