3205d7d05e5ccee0f51620891771a9b058958276
Michael Ornellas <mike@themakersquare.com>
2013-10-10 00:18:49 -0500

    stubbed model descriptions

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -14,14 +14,14 @@ describe Movie do
 
     context "when description is less than or equal to 50 characters" do
       it "returns the full description" do
-        movie.description = "When a Roman general is betrayed and his family is"
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family is")
         expect(movie.snippet).to eq(movie.description)
       end
     end
 
     context "when description is greater than 50 characters" do
       it "returns the first 47 characters and appends '...'" do
-        movie.description = "When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge."
+        allow(movie).to receive(:description).and_return("When a Roman general is betrayed and his family murdered by an emperor's corrupt son, he comes to Rome as a gladiator to seek revenge.")
         expect(movie.snippet).to eq("When a Roman general is betrayed and his family...")
       end
     end
