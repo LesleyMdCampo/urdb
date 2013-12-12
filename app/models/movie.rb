bdeebc9d3434c8241d218feaabe6ae6bc5d35b40
LesleyMdCampo <lesliz32@gmail.com>
2013-12-12 17:17:06 -0600

    Add passing tests for class method average_rating.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -23,7 +23,22 @@ class Movie < ActiveRecord::Base
   end
 
   def self.average_rating
+  	#get all movie scores
+  	scores = self.all.collect do |movie|
+  	  movie.audience_rating
+  	end
+  	scores.compact!
+  	#add all movie scores together
+  	sum = scores.sum
+  	#divide by number of movies
+  	if  scores.length == 0
+  		return nil
+  	else
+  		average = sum/scores.length
+  		return average
+  	end
 
+  	
   end
 end
 
